39246a9af30abdd2a87448caf9077a07f5d1698a
P Prakash <prakashguru@users.noreply.github.com>
2021-07-24 11:07:12 +0530

    Further Changes

--- Program.cs
+++ Program.cs
@@ -13,31 +13,15 @@ namespace NlogIssue
             
             NLog.LogManager.Setup().SetupSerialization(d => 
                 d.RegisterObjectTransformation<StudentData>(data => new StudentData(data.Name  + "-Modified", data.Id + "-changed" ))
-                    .RegisterObjectTransformation<StudentDataN>(data => new StudentDataN(data.Name  + "-N Modified", data.Id + "- N changed" ))
+                    
                 );
             
             logger.Log(LogLevel.Info, "Student Data 1 {@data}", new StudentData("Jack", "10"));
-            logger.Log(LogLevel.Info, "Student Data 2 {@data}", new StudentData("Jack", "10"));
+            logger.Log(LogLevel.Info, "Student Data 2 {@data}", new StudentData("Jack3", "11"));
 
-            logger.Log(LogLevel.Info, "Student DataN 3 {@data}", new StudentDataN("Jack", "10"));
-            logger.Log(LogLevel.Info, "Student DataN 4 {@data}", new StudentDataN("Jack", "10"));
-
-            
-            Console.ReadLine();
         }
         
         public record StudentData(string Name, string Id);
 
-        public class StudentDataN
-        {
-            public StudentDataN(string Name, string Id)
-            {
-                this.Name = Name;
-                this.Id = Id;
-            }
-
-            public string Name { get; set; }
-            public string Id { get; set; }
-        }
     }
 }
